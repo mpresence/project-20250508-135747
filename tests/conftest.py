ff8e6d653b1dedd53f276a79298269bb3265db60
mpresence <kofi.glover@gmail.com>
2025-05-08 14:02:37 +0000

    Add tests/conftest.py

---
+++ tests/conftest.py
@@ -0,0 +1,19 @@
+import pytest
+from calculator_app import create_app
+from calculator_app.calculator import Calculator
+
+@pytest.fixture
+def app():
+    app = create_app()
+    app.config.update({
+        "TESTING": True,
+    })
+    yield app
+
+@pytest.fixture
+def client(app):
+    return app.test_client()
+
+@pytest.fixture
+def calculator():
+    return Calculator()
